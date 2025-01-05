46ad2fceaad140160b529c1be157f51057b0ea1d
iNewHereOnGit <mmblessbrian@gmail.com>
2025-01-05 09:29:38 -0500

    comment

--- utilities/inputReader.js
+++ utilities/inputReader.js
@@ -1,11 +1,12 @@
 import * as readline from "node:readline/promises";
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
 });
 
+//prompts user for input
 const getInput = async (question) => {
-    return rl.question(question);
+	return rl.question(question);
 };
 
 export { getInput };
