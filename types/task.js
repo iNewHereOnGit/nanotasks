100c4264fa537bd1f93a2f1d410f69df9d5f0049
iNewHere <66796801+mmblessbrian@users.noreply.github.com>
2025-01-09 21:32:21 -0500

    add due date seconds column to tasks table and into Task class

--- types/task.js
+++ types/task.js
@@ -1,10 +1,17 @@
 class Task {
-    constructor(name, description, priority = 0, completed = 0) {
-        this.name = name;
-        this.description = description;
-        this.priority = priority;
-        this.completed = completed;
-    }
+	constructor(
+		name,
+		description,
+		priority = 0,
+		completed = 0,
+		dueDate
+	) {
+		this.name = name;
+		this.description = description;
+		this.priority = priority;
+		this.completed = completed;
+		this.dueDate = dueDate;
+	}
 }
 
 export { Task };
--- utilities/database.js
+++ utilities/database.js
@@ -4,11 +4,12 @@ const db = new Database('tasks.db');
 //initialize tasks table using prepared statement
 try {
 	db.prepare(
-		'CREATE TABLE IF NOT EXISTS "tasks" ( "id" INTEGER NOT NULL UNIQUE, "title" TEXT NOT NULL, "description" TEXT, "priority" INTEGER DEFAULT 0 CHECK(priority >= 0 AND priority <= 3), "completed" INTEGER NOT NULL DEFAULT 0 CHECK(completed = 0 OR completed = 1), PRIMARY KEY("id" AUTOINCREMENT))'
+		'CREATE TABLE IF NOT EXISTS "tasks" ( "id" INTEGER NOT NULL UNIQUE, "title" TEXT NOT NULL, "description" TEXT, "priority" INTEGER DEFAULT 0 CHECK(priority >= 0 AND priority <= 3), "completed" INTEGER NOT NULL DEFAULT 0 CHECK(completed = 0 OR completed = 1), "dueDateSeconds" INTEGER NOT NULL DEFAULT -1, PRIMARY KEY("id" AUTOINCREMENT))'
 	).run();
 } catch (error) {
 	console.log(
-		`[ERROR] Failed to initialize Tasks table. Does the database already exist?`
+		`[ERROR] Failed to initialize Tasks table. Does the database already exist?`,
+		error
 	);
 	process.exit(-1);
 }
