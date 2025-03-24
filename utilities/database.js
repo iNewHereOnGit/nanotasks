66d667cd420034dfa4bf70c63a09cf8f87983f1a
iNewHereOnGit <mmblessbrian@gmail.com>
2025-03-23 21:59:24 -0400

    DB SCHEMA - create lists table

--- utilities/database.js
+++ utilities/database.js
@@ -4,7 +4,13 @@ const db = new Database("tasks.db");
 //initialize tasks table using prepared statement
 try {
 	db.prepare(
-		'CREATE TABLE IF NOT EXISTS "tasks" ( "id" INTEGER NOT NULL UNIQUE, "title" TEXT NOT NULL, "note" TEXT, "tag" TEXT, "priority" INTEGER DEFAULT 0 CHECK(priority >= 0 AND priority <= 3), "completed" INTEGER NOT NULL DEFAULT 0 CHECK(completed = 0 OR completed = 1), "due" INTEGER NOT NULL DEFAULT -1, "created" INTEGER NOT NULL, "modified" INTEGER NOT NULL, PRIMARY KEY("id" AUTOINCREMENT))'
+		'CREATE TABLE IF NOT EXISTS "tasks" ( "id" INTEGER NOT NULL UNIQUE, "title" TEXT NOT NULL, "note" TEXT, "tag" TEXT, "priority" INTEGER DEFAULT 0 CHECK(priority >= 0 AND priority <= 3), "completed" INTEGER NOT NULL DEFAULT 0 CHECK(completed = 0 OR completed = 1), "due" INTEGER NOT NULL DEFAULT -1, "created" INTEGER NOT NULL, "modified" INTEGER NOT NULL, "list" TEXT, PRIMARY KEY("id" AUTOINCREMENT))'
+	).run();
+
+	//initialize lists table
+	db.prepare(
+		`CREATE TABLE IF NOT EXISTS "lists" ("id" INTEGER NOT NULL UNIQUE,"name" TEXT NOT NULL,"description" TEXT, "tag" TEXT,"created"	INTEGER NOT NULL, "modified" INTEGER NOT NULL,"deleted"	INTEGER NOT NULL DEFAULT 0,"type"TEXT NOT NULL DEFAULT "standard", PRIMARY KEY("id" AUTOINCREMENT))
+	`
 	).run();
 } catch (error) {
 	console.log(
